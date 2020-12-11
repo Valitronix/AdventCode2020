3a5bcf8fa53fd353b95101e68a49140f892b0222
Valitronix <arvaliton@wpi.edu>
2020-12-11 16:01:23 -0500

    Cleaned up print statements for Day 11

--- Day11/modelSeatSelection.py
+++ Day11/modelSeatSelection.py
@@ -2,29 +2,22 @@ def countNeighbors(seat_map, row, index):
     count = 0
     for ri in range(max(0, row-1),min(len(seat_map), row+2)):
         for ci in range(max(0, index-1),min(len(seat_map[0]), index+2)):
-            #print("Checking row", ri, ", seat", ci, ", which is a", seat_map[ri][ci])
             if ri == row and ci == index:
-                #print("whoops that's the current cell")
                 continue
             elif seat_map[ri][ci] == '#':
-                # print("it's a #! count++ ")
                 count += 1
-            #print("it's a", seat_map[ri][ci])
     return count
 
 def goOneRound(seat_map, new_map):
     for ri, row in enumerate(seat_map):
         for ci, chair in enumerate(row):
-            #print("Checking row", ri, ", seat", ci, "which is a", chair)
             if chair == 'L' and countNeighbors(seat_map, ri, ci) == 0:
-                #print("No neighbors, changing to #")
                 new_map[ri][ci] = '#'
             elif chair == '#' and countNeighbors(seat_map, ri, ci) >= 4:
-                #print("Too many neighbors, changing to L")
                 new_map[ri][ci] = 'L'
             else:
                 new_map[ri][ci] = chair
-    return new_map # [''.join(row) for row in new_map]
+    return new_map 
 
 def countOccupiedSeats(seat_map):
     count = 0
@@ -42,13 +35,9 @@ new_map = [[''] * len(seat_map[0]) for _ in range(len(seat_map))]
 new_map = goOneRound(seat_map, new_map)
 iter_count = 1
 
-# print("After", iter_count, "iterations, the seat map is:")
-# print('\n'.join([''.join(row) for row in new_map]))
 while new_map != seat_map:
     seat_map = new_map.copy()
     new_map = [[''] * len(seat_map[0]) for _ in range(len(seat_map))]
-    # print("Starting with seat map:")
-    # print('\n'.join([''.join(row) for row in seat_map]))
     new_map = goOneRound(seat_map, new_map)
     iter_count += 1
 
@@ -57,6 +46,3 @@ print('\n'.join([''.join(row) for row in new_map]))
 
 print("The number of occupied chairs is", countOccupiedSeats(seat_map))
 
-# print(len(seat_map))
-# print(seat_map[0][2])
-# print('\n'.join(seat_map))
